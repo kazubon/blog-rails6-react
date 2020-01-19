bd13e9c29d7458744aa1b1de583d37032d07b8a6
Kazuto Sato <kazubonx@gmail.com>
2020-01-19 12:51:13 +0900

    Vue.js外す

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
 
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-// Vue.js フル版（Compiler入り）
-environment.config.resolve.alias = { 'vue$': 'vue/dist/vue.esm.js' }
 module.exports = environment
