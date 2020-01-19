864f457d634125bf4762e650dcc9b1a659f570f8
Kazuto Sato <kazubonx@gmail.com>
2020-01-19 12:54:33 +0900

    微修正: search_form.js

--- app/javascript/entries/search_form.js
+++ app/javascript/entries/search_form.js
@@ -5,23 +5,15 @@ export default function (props) {
   const [title, setTitle] = useState(query.title || '');
   const [tag, setTag] = useState(query.tag || ''  );
 
-  function handleTitle(e) {
-    setTitle(e.target.value);
-  }
-
-  function handleTag(e) {
-    setTag(e.target.value);
-  }
-
   return (
     <div>
       <form action="/entries" method="get" className="form-inline mb-4">
         <input type="text" name="title" className="form-control mr-3 mb-2"
           placeholder="タイトル"
-          value={title} onChange={handleTitle} />
+          value={title} onChange={e => setTitle(e.target.value)} />
         <input type="text" name="tag" className="form-control mr-3 mb-2"
           placeholder="タグ"
-          value={tag} onChange={handleTag} />
+          value={tag} onChange={e => setTag(e.target.value)} />
         <input type="hidden" name="sort" value={query.sort || '' } />
         <input type="hidden" name="user_id" value={query.user_id || '' } />
         <button type="submit" className="btn btn-outline-primary mb-2">検索</button>
