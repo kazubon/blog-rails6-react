11d104509d9f16f1e096ba03f02c129e252ad716
Kazuto Sato <kazubonx@gmail.com>
2020-01-25 16:44:49 +0900

    babel.config.js: useBuiltIns: 'entry' に戻す

--- babel.config.js
+++ babel.config.js
@@ -31,7 +31,7 @@ module.exports = function(api) {
         '@babel/preset-env',
         {
           forceAllTransforms: true,
-          useBuiltIns: 'usage',
+          useBuiltIns: 'entry',
           corejs: 3,
           modules: false,
           exclude: ['transform-typeof-symbol']
